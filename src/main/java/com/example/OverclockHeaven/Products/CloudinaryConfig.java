4c08bf24eea971dda6a29741e4c890588a8dce5a
Sípos Szabolcs <siposszabolcs10@gmail.com>
2024-11-01 11:36:55 +0100

    Real final commit

--- src/main/java/com/example/OverclockHeaven/Products/CloudinaryConfig.java
+++ src/main/java/com/example/OverclockHeaven/Products/CloudinaryConfig.java
@@ -2,18 +2,29 @@ package com.example.OverclockHeaven.Products;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
 public class CloudinaryConfig {
+
+    @Value("${CLOUDINARY_CLOUD_NAME}")
+    private String cloudName;
+
+    @Value("${CLOUDINARY_API_KEY}")
+    private String apiKey;
+
+    @Value("${CLOUDINARY_API_SECRET}")
+    private String apiSecret;
+
     @Bean
     public Cloudinary cloudinary() {
         return new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", "deea2iows",
-                "api_key", "353872364595939",
-                "api_secret", "whxYV-lKWN7Li9I8I13gCdfmbag"
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret
         ));
     }
 }
